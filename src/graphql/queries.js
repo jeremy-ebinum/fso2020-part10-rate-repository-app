79ef70d27468b04a3473556f53558bdf71854b55
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2020-09-29 16:01:56 +0100

    feat: fetch repositories with graphql api

---
+++ src/graphql/queries.js
@@ -0,0 +1,28 @@
+import { gql } from "apollo-boost";
+
+const REPOSITORY_DETAILS = gql`
+  fragment repositoryDetails on Repository {
+    id
+    fullName
+    description
+    language
+    forksCount
+    stargazersCount
+    ratingAverage
+    reviewCount
+    ownerAvatarUrl
+  }
+`;
+
+export const GET_REPOSITORIES = gql`
+  query getRepositories {
+    repositories {
+      edges {
+        node {
+          ...repositoryDetails
+        }
+      }
+    }
+  }
+  ${REPOSITORY_DETAILS}
+`;
---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const getRepositories = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
+
+  const { called, networkStatus, loading, data, refetch } = getRepositories;
+
+  useEffect(() => {
+    if (called & (networkStatus > 6)) {
+      const fetchedRepositories = data ? data.repositories : [];
+      setRepositories(fetchedRepositories);
+    }
+  }, [getRepositories]);
+
+  return { repositories, loading, refetch };
+};
+
+export default useRepositories;
